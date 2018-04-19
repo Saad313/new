a2510ba73457db79cddffd7fe46e10dc324be11b
mzraz <mzraza512@gmail.com>
2018-04-18 23:27:34 -0700

    server
    a

---
+++ WcfService3/WcfService3/userclass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfService3
+{
+    public class userclass
+    {
+        private string username;
+        private string password;
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+    }
+}
