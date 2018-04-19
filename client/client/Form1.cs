28245384ccfde0388d2990930069a93c713cc2e8
Saad313 <m.saadrehman313@gmail.com>
2018-04-19 11:57:09 +0500

    saad

---
+++ client/client/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class Register : Form
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdregister_Click(object sender, EventArgs e)
+        {
+            localhost.Service1 server = new localhost.Service1();
+            server.register(txtusername.Text , txtpassword.Text);
+            if (string.IsNullOrWhiteSpace(txtusername.Text) || string.IsNullOrWhiteSpace(txtpassword.Text))
+            {
+                MessageBox.Show("Empty");
+            }
+            else
+            {
+                if (chkadmin.CheckState == CheckState.Checked)
+                {
+                    MessageBox.Show("Registered As Admin");
+                }
+                else 
+                {
+                    MessageBox.Show("Registered");
+                }
+            }
+        }
+
+        private void lbllogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Login f = new Login();
+            this.Hide();
+            f.Show();
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkadmin_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ client/client/Login.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdlogin_Click(object sender, EventArgs e)
+        {
+            bool valid;
+            bool isvalid;
+            localhost.Service1 server = new localhost.Service1();
+            server.Login(txtusername.Text, txtpassword.Text ,out valid, out isvalid);
+            if (valid)
+            {
+                MessageBox.Show("Welcome"); 
+            }
+            else 
+            {
+                MessageBox.Show("getout"); 
+            }
+            
+        }
+
+        private void lblregister_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Register R = new Register();
+            this.Hide();
+            R.Show();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
